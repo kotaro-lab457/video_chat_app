a5ff750d8b515b87dd60278577773a0292523b86
鈴木孝太郎 <k.chanmpmp@gmail.com>
2021-08-17 00:08:42 +0900

    add: カメラの表示

--- public/script.js
+++ public/script.js
@@ -2,6 +2,28 @@
 const socket = io("/");
 
 const myPeer = new Peer();
+const videoWrap = document.getElementById("video-wrap");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+const addVideoStream = (video, stream) => {
+  // DOM要素（video）
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    // play()メソッド:再生する
+    video.play();
+  });
+  videoWrap.append(video);
+}
+
+// デバイスから音声オーディオやビデオデータの取得
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+}).then((stream) => {
+  // 第一引数にDOM、第二引数に情報を格納
+  addVideoStream(myVideo, stream);
+});
 
 // openイベントの受信
 myPeer.on("open", userId => {
